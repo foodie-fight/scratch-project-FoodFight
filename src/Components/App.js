1ce51ceaff1c8f5eca15f5d816d0b33f291e1ec4
William <he.william@yahoo.com>
2017-08-25 09:36:35 -0700

    created components

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import io from 'socket.io-client';
+import Options from './Options.js'
 
 
 
@@ -9,7 +10,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                {io.connect('http://localhost:3000')}
+                <Options />
             </div>
         )
     }

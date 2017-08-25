fc9f8060d6c9c01295a032e2b1e55acdaebf7c8d
William <he.william@yahoo.com>
2017-08-24 18:23:28 -0700

    added boilerplate

---
+++ src/Components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>BLQHH</div>
+        )
+    }
+}
+
+
+export default App;

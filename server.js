e12e0d038b7e941eeb6425dacb52894b3ff6df8d
Kristopherlb <kristopherl.bowles@gmail.com>
2017-08-25 21:24:15 -0700

    just throwing them in for update

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-  
+
 const express = require('express');
 const app = express();
-const server = require('http').Server(app);
-const socket = require('socket.io');
-const io = socket(server);
+const server = app.listen(3000);
+const io = require('socket.io').listen(server);
+const users = [];
+const title = '';
 const fs = require('fs');
 
 
 app.use(express.static(__dirname + '/www'));
-
-
-io.on('connect', function () {
-  console.log('connected')
-})
-
-
-
-server.listen(3000)
+io.sockets.on('connect', function (socket) {
+  console.log('connected');
+  socket.emit('welcome', {
+    title: title
+  });
+  users.push(socket);
+  console.log('Connected: %s users', users.length);
+  // console.log('socket:  ', socket);
+  console.log('socket id:  ', socket.id);
+});
